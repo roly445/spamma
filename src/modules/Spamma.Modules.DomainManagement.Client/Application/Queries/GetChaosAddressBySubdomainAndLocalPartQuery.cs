8add93738b71cd715c91107f482c27dde8d94e5f
Andrew Davis <andrew@deviouscreation.com>
2025-11-02 17:09:00 +0000

    fix: remove BluQubeQuery attribute from GetChaosAddressBySubdomainAndLocalPartQuery
    This query is only used server-side in SpammaMessageStore during SMTP processing.
    It doesn't need BluQube code generation since it's never called from the UI.
    The attribute was causing 405 errors on non-existent endpoint.

--- src/modules/Spamma.Modules.DomainManagement.Client/Application/Queries/GetChaosAddressBySubdomainAndLocalPartQuery.cs
+++ src/modules/Spamma.Modules.DomainManagement.Client/Application/Queries/GetChaosAddressBySubdomainAndLocalPartQuery.cs
@@ -1,7 +1,5 @@
-using BluQube.Attributes;
 using BluQube.Queries;
 
 namespace Spamma.Modules.DomainManagement.Client.Application.Queries;
 
-[BluQubeQuery(Path = "api/domain-management/chaos-addresses/find")]
 public record GetChaosAddressBySubdomainAndLocalPartQuery(Guid SubdomainId, string LocalPart) : IQuery<GetChaosAddressBySubdomainAndLocalPartQueryResult>;
